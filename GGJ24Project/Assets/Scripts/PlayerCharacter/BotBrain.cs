f5208eeac7a58ae69b49740baf3fdfa27f545e69
cnbrenci <cnbrenci@gmail.com>
2024-01-27 12:23:35 -0800

    Make the bot baby hug while chasing

--- GGJ24Project/Assets/Scripts/PlayerCharacter/BotBrain.cs
+++ GGJ24Project/Assets/Scripts/PlayerCharacter/BotBrain.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 
 namespace LeftOut.GameJam
@@ -10,17 +13,61 @@ namespace LeftOut.GameJam
         private RagdollCharacter _self;
         private RagdollLocomotion _locomotion;
         private RagdollCombat _combat;
+        private float _lastHug;
+        private float _lastMove;
+        
+        private const float HugCooldown = .5f;
+        
         // Start is called before the first frame update
         void Start()
         {
-            
             _self = GetComponent<RagdollCharacter>();
             _combat = GetComponent<RagdollCombat>();
             _locomotion = GetComponent<RagdollLocomotion>();
+            _lastHug = Time.time - HugCooldown;
         }
 
         // Update is called once per frame
         void Update()
+        {
+            var targetBaby = FindClosestBabyOrNull();
+            if (!targetBaby)
+            {
+                return;
+            }
+
+            var toBaby = targetBaby.transform.position - transform.position;
+            var distToBaby = toBaby.magnitude;
+
+            // chase the baby!
+            if (distToBaby > 2f)
+            {
+                // TODO: This only starts movement and it never stops
+                Debug.Log($"distance is {distToBaby}");
+                Debug.Log($"Chase the baby: mag {toBaby.magnitude} xyz:{toBaby}");
+                _locomotion.SetMoveGlobal(toBaby);
+            }
+
+            // hug the baby!
+            // Debug.Log($"distance is {toBaby.magnitude}");
+            if (distToBaby < 1f && Time.time - _lastHug > HugCooldown)
+            {
+                Debug.Log($"distance is {distToBaby}");
+                Debug.Log("Hug!");
+                Hug();
+            }
+            
+            // TODO: spit up at the baby!
+        }
+
+        private void Hug()
+        {
+            _lastHug = Time.time;
+            _combat.TryPerform(AttackType.SlapLeft);
+            _combat.TryPerform(AttackType.SlapRight);
+        }
+
+        private RagdollCharacter FindClosestBabyOrNull()
         {
             float minDistance = float.MaxValue;
             RagdollCharacter closestBaby = null;
@@ -36,14 +83,7 @@ namespace LeftOut.GameJam
                 }
             }
 
-            if (!closestBaby)
-            {
-                return;
-            }
-
-            var toBaby = closestBaby.transform.position - transform.position;
-
-            _locomotion.SetMoveGlobal(toBaby);
+            return closestBaby;
         }
     }
 }
