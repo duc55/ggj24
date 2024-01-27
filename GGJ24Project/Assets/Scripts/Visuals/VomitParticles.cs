1ba0d7cc3ed0a535511c21bc0f8649388de382c3
Jaden Goter <jadengoter@gmail.com>
2024-01-27 15:59:42 -0800

    Fix vomit, add Start and Stop functions

--- GGJ24Project/Assets/Scripts/Visuals/VomitParticles.cs
+++ GGJ24Project/Assets/Scripts/Visuals/VomitParticles.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 namespace LeftOut.GameJam
 {
+    [RequireComponent(typeof(ParticleSystem))]
     public class VomitParticles : MonoBehaviour
     {
+        private ParticleSystem _ps;
+
+        public void StartVomit()
+        {
+            _ps.Play();
+        }
+
+        public void StopVomit()
+        {
+            _ps.Stop();
+        }
+
+        private void Awake()
+        {
+            _ps = GetComponent<ParticleSystem>();
+        }
+
         private void OnParticleCollision(GameObject other)
         {
             if (other.name.StartsWith("foot"))
